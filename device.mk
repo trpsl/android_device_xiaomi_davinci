43a21b41881944467de1323f9fd35b9c10cf5a8f
trpsl <trpsl755@gmail.com>
2020-07-17 16:15:19 +0200

    davinci: Reorder overlays

--- device.mk
+++ device.mk
@@ -11,11 +11,9 @@ $(call inherit-product-if-exists, vendor/xiaomi/davinci/davinci-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
     $(LOCAL_PATH)/overlay/frameworks/base/packages/SystemUI
 
 # Permissions
